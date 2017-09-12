8ad189b5c64a13d34a7d0b0004cc64e0177d65f6
KingPra <kingi.pra@gmail.com>
2017-09-11 20:14:29 -0400

    pulling in random quotes

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('hiya');
+
+window.addEventListener('load', () => {
+  getQuotes();
+});
+
+let button = document.querySelector('.get_quote');
+button.addEventListener('click', () => getQuotes())
+
+function getQuotes () {
+  let request = new XMLHttpRequest();
+  request.open('GET', 'https://talaikis.com/api/quotes/random/');
+  request.addEventListener('load', () => {
+    let response = JSON.parse(request.responseText);
+    let quote = document.querySelector('.quote');
+    let author = document.querySelector('.author');
+
+    quote.innerHTML = response.quote;
+    author.innerHTML = `- ${response.author}`;
+    console.log(response);
+  })
+request.send();
+}
--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,25 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-console.log('hiya');
+
+window.addEventListener('load', () => {
+  getQuotes();
+});
+
+let button = document.querySelector('.get_quote');
+button.addEventListener('click', () => getQuotes())
+
+function getQuotes () {
+  let request = new XMLHttpRequest();
+  request.open('GET', 'https://talaikis.com/api/quotes/random/');
+  request.addEventListener('load', () => {
+    let response = JSON.parse(request.responseText);
+    let quote = document.querySelector('.quote');
+    let author = document.querySelector('.author');
+
+    quote.innerHTML = response.quote;
+    author.innerHTML = `- ${response.author}`;
+    console.log(response);
+  })
+request.send();
+}
 
 },{}]},{},[1]);
